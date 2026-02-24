9709f8dbe046c43a8ead4639449265a3af24b7f0
Florea Andrei <florea3005@gmail.com>
2026-02-24 21:23:58 +0100

    Added Settings, edited Example Screen(Doesn't mattter) Testing

--- example.py
+++ example.py
@@ -5,4 +5,11 @@ screen = pygame.display.set_mode((800,600))
 def example_screen_function():
     #this opens a window with the color red and names it
     pygame.display.set_caption("Kar Kart")
-    screen.fill((255, 0, 0))
+    screen.fill((50, 150, 50))
+
+    font = pygame.font.SysFont("arial", 48)
+    text = font.render("This is an example screen", True, (255, 255, 255))
+    text_rect = text.get_rect(center=(400, 300))
+    screen.blit(text, text_rect)
+
+    return "test"
--- main.py
+++ main.py
@@ -28,8 +28,50 @@ def example_screen_function():
 
     screen.fill(DARK)
 
+    title_surf = FONT_TITLE.render("Kar Kart", True, WHITE)
+    title_rect = title_surf.get_rect(center=(400, 180))
+    screen.blit(title_surf, title_rect)
 
+    play_btn_rect = pygame.Rect(0, 0, 220, 70)
+    play_btn_rect.center = (400, 350)
 
+    if play_btn_rect.collidepoint(mouse_pos):
+        color = BTN_HOVER
+    else:
+        color = BTN_COLOR
+
+    # Play Button - EVERYTHING STILL IN TESTS
+
+    pygame.draw.rect(screen,color, play_btn_rect, border_radius= 12)
+
+    play_surf = FONT_BTN.render("Play", True, WHITE)
+    play_text_rect = play_surf.get_rect(center = play_btn_rect.center)
+    screen.blit(play_surf, play_text_rect)
+
+    if play_btn_rect.collidepoint(mouse_pos) and mouse_pressed:
+        return "test"   # this will change the state to the test state, which is the example screen in example.py
+
+    # Settings Button - EVERYTHING STILL IN TESTS
+
+    settings_btn_rect = pygame.Rect(0, 0, 220, 70)
+    settings_btn_rect.center = (400, 450) # under play button
+    settings_color = BTN_HOVER if settings_btn_rect.collidepoint(mouse_pos) else BTN_COLOR
+    
+    pygame.draw.rect(screen, settings_color, settings_btn_rect, border_radius=12)
+
+    settings_surf = FONT_BTN.render("Settings", True, WHITE)
+    settings_text_rect = settings_surf.get_rect(center = settings_btn_rect.center)
+    screen.blit(settings_surf, settings_text_rect)
+
+    if settings_btn_rect.collidepoint(mouse_pos) and mouse_pressed:
+        print("Settings!") # Temporary
+        return "settings"   # this will change the state to the settings state
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_ESCAPE]:
+        return "test"
+    
+    return "start"
 
 
 def main():
@@ -44,7 +86,7 @@ def main():
 
 
         if game_state == "start":
-            example_screen_function()
+            game_state =example_screen_function()
         elif game_state == "test":
             example.example_screen_function()
 
