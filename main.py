9082307821dcf4677a1fc38e6548a4df717e4a77
Florea Andrei <florea3005@gmail.com>
2026-02-24 20:57:42 +0100

    Work in example screen

--- main.py
+++ main.py
@@ -15,7 +15,20 @@ screen = pygame.display.set_mode((800,600))    #please if you change the screen 
 def example_screen_function():
     #this opens a window with the color grey and names it
     pygame.display.set_caption("Kar Kart")
-    screen.fill((30, 30, 30))
+    FONT_TITLE = pygame.font.SysFont("arial", 64, bold=True)
+    FONT_BTN = pygame.font.SysFont("arial",32)
+
+    WHITE = (255, 255, 255)
+    DARK = (20, 20, 30)
+    BTN_COLOR = (70, 130, 180)
+    BTN_HOVER = (100, 170, 220)
+
+    mouse_pos = pygame.mouse.get_pos()
+    mouse_pressed = pygame.mouse.get_pressed()[0] # if the left mouse click is pressed
+
+    screen.fill(DARK)
+
+
 
 
 
