1594264d7e7aaee7e6bd485bd77ff3366f251f72
NGL07 <melektuzlu07@gmail.com>
2026-02-27 14:07:25 +0100

    Added car selection screen. Note: 'back', 'next', and 'settings' buttons will be added

--- screens/car_selection_screen.py
+++ screens/car_selection_screen.py
@@ -9,25 +9,68 @@ from constants import ScreenDimensions as sd
 class CarScreen:
     def __init__(self, manager):
         self.manager = manager     # ALWAYS ADD THE MANAGER
-        self.button1 = Button(sd.CENTER_X, sd.CENTER_Y, 100, 50, "Red", "map", self.manager)
+
+        
+        #load all images together
+        self.background = pygame.transform.scale( pygame.image.load("cust1.png").convert(), (sd.WIDTH, sd.HEIGHT) )
+
+        self.car_images = [ 
+            pygame.transform.scale(pygame.image.load("amv.png").convert_alpha(), (360, 183)), 
+            pygame.transform.scale(pygame.image.load("ja.png").convert_alpha(), (360, 261)), 
+            pygame.transform.scale(pygame.image.load("tm.png").convert_alpha(), (360, 129)), 
+        ]
+
+        self.selected = 0  #default car
+
+
 
     def handle_event(self, event):  # use this template for the key detection
 
-        # ask button1 to handle event
-        
-        self.button1.handle_event(event)
+        if event.type == pygame.KEYDOWN: 
+            if event.key == pygame.K_RIGHT: 
+                self.selected = min(len(self.car_images) - 1, self.selected + 1) 
+            elif event.key == pygame.K_LEFT: 
+                self.selected = max(0, self.selected - 1) 
+                
+            # TO DO: go to map screen 
+            # if 'next' button is clicked: 
+                # self.manager.change_screen("map")
+
+            # TO DO: go to start screen 
+            # if 'back' button is clicked: 
+                # self.manager.change_screen("start")
 
     def update(self): # add any other object here like the car class as well as its physics
         # example:
         # car.y += 5
-        pass
+        pass # no physics yet
 
     def draw(self, surface): # use this function to draw anything onto the screen
-        
         # fill surface + call draw for all the objects inside
 
-        pygame.display.set_caption("Kar Kart")
+        pygame.display.set_caption("Car Selection")
 
-        surface.fill((0, 0, 175))
+        # draw background 
+        surface.blit(self.background, (0, 0))
 
-        self.button1.draw(surface)
+        # draw selected car in the center 
+        car = self.car_images[self.selected] 
+        x = sd.WIDTH // 2 - car.get_width() // 2 
+        y = sd.HEIGHT // 2 - car.get_height() // 2 
+        surface.blit(car, (x, y))
+
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
