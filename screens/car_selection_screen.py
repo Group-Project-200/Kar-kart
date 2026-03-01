db4ac927e25a36820024d19b95f6537504efdf06
NGL07 <melektuzlu07@gmail.com>
2026-03-01 06:49:44 +0100

    replaced car placeholder images with real cars(pixel-layered). added cust1.png(background img). added buttons(back & next) to navigate between screens. added CarButton class in ui/button.py to use button in different color. added comments(line 8-12) showing what needs to be modified in game-engine/render.py to make the background of car preview transparent.

--- screens/car_selection_screen.py
+++ screens/car_selection_screen.py
@@ -1,29 +1,73 @@
-# car_customization_screen.py - screen to choose the map
-# remove comments under ### 
+# car_customization_screen.py - screen to choose the car
+
+# in same working directory requires:
+    # render.py
+    # cust1.png (background img)
+    # resources folder with car_01, car_02... folders which contain car layers img_0.png, img_1.png,...
+
+# changes in render.py:
+# to make the background of car preview transparent:
+    # replaced PREVIEW_DEBUG_BG_COLOR = (96, 96, 96) with PREVIEW_DEBUG_BG_COLOR = (0, 0, 0, 0)
+    # replaced frame_surface.fill(PREVIEW_DEBUG_BG_COLOR) with frame_surface.fill((0, 0, 0, 0)) 
+    # changed frame_surface = pygame.Surface(render_size).convert() with frame_surface = pygame.Surface(render_size, pygame.SRCALPHA).convert_alpha()
+
+# TO ADD: settings button
+
+
 
 import pygame
 
-from ui.button import Button
+from ui.button import CarButton
 from constants import Colors
 from constants import ScreenDimensions as sd
+
+from render import build_render_pipeline, render_preview_debug_frame, RenderSetup
+import os
+
 
 class CarScreen:
     def __init__(self, manager):
         self.manager = manager     # ALWAYS ADD THE MANAGER
 
-        ### initialize buttons
-        #self.next_btn = Button(sd.CENTER_X, sd.CENTER_Y, 200, 75, "ORANGE", "NEXT", self.manager)
-        #self.back_btn = Button(sd.CENTER_X, sd.CENTER_Y, 100, 50, "BACK", "map", self.manager)
-        #self.settings_btn = Button(sd.CENTER_X, sd.CENTER_Y, 100, 50, "SETTINGS", "map", self.manager)
+        # initialize buttons  
+        #* states will be changed to "map" and "start" when they're implemented
+        self.next_btn = CarButton(560, 535, 150, 50, "NEXT", "car", self.manager)
+        self.back_btn = CarButton(240, 535, 150, 50, "BACK", "car", self.manager)
         
-        #load all images together
-        self.background = pygame.transform.scale( pygame.image.load("car_selection_images/cust1.png").convert(), (sd.WIDTH, sd.HEIGHT) )
+        
+        # load all images together
+        self.background = pygame.transform.scale( pygame.image.load("cust1.png").convert(), (sd.WIDTH, sd.HEIGHT) )
 
-        self.car_images = [ 
-            pygame.transform.scale(pygame.image.load("car_selection_images/amv.png").convert_alpha(), (360, 183)), 
-            pygame.transform.scale(pygame.image.load("car_selection_images/ja.png").convert_alpha(), (360, 261)), 
-            pygame.transform.scale(pygame.image.load("car_selection_images/tm.png").convert_alpha(), (360, 129)), 
+        def load_car_slices(folder):
+            files = sorted(os.listdir(folder))
+            return [pygame.image.load(os.path.join(folder, f)).convert_alpha() for f in files]
+
+        # load all cars from resources
+        self.car_slices = [
+            load_car_slices("resources/car_01"),
+            load_car_slices("resources/car_02"),
+            load_car_slices("resources/car_03"),
+            load_car_slices("resources/car_04"),
         ]
+
+        self.pipelines = []
+        for slices in self.car_slices:
+            pipeline = build_render_pipeline(
+                screen_size=(600, 450),   # preview size
+                map_surface=None,
+                image_stack=slices,
+                setup=RenderSetup(
+                    map_zoom=1.0,
+                    # car size
+                    car_zoom=9.0,
+                    pixelation_scale=1.0
+                ),
+                dirs=36
+            )
+            self.pipelines.append(pipeline)
+        
+        self.preview_angle = 0
+
 
         self.selected = 0  #default car
 
@@ -31,24 +75,16 @@ class CarScreen:
 
     def handle_event(self, event):  # use this template for the key detection
 
-        ### ask buttons to handle event        
-        #self.next_btn.handle_event(event)
-        #self.back_btn.handle_event(event)
-        #self.settings_btn.handle_event(event)
+        # ask buttons to handle event        
+        self.next_btn.handle_event(event)
+        self.back_btn.handle_event(event)
 
         if event.type == pygame.KEYDOWN: 
             if event.key == pygame.K_RIGHT: 
-                self.selected = min(len(self.car_images) - 1, self.selected + 1) 
+                self.selected = min(len(self.car_slices) - 1, self.selected + 1) 
             elif event.key == pygame.K_LEFT: 
                 self.selected = max(0, self.selected - 1) 
-                
-            # TO DO: go to map screen 
-            # if 'next' button is clicked: 
-                # self.manager.change_screen("map")
 
-            # TO DO: go to start screen 
-            # if 'back' button is clicked: 
-                # self.manager.change_screen("start")
 
     def update(self): # add any other object here like the car class as well as its physics
         # example:
@@ -63,20 +99,28 @@ class CarScreen:
         # draw background 
         surface.blit(self.background, (0, 0))
 
-        # draw selected car in the center 
-        car = self.car_images[self.selected] 
-        x = sd.WIDTH // 2 - car.get_width() // 2 
-        y = sd.HEIGHT // 2 - car.get_height() // 2 
-        surface.blit(car, (x, y))
+        pipeline = self.pipelines[self.selected]
 
-        ### draw buttons
-        #self.next_btn.draw(surface)
-        #self.back_btn.draw(surface)
-        #self.settings_btn.draw(surface)
+        # slowly rotate the preview
+        self.preview_angle = (self.preview_angle + 1) % 360
+
+        # center the preview of car 
+        preview_surface = surface.subsurface(
+        (sd.WIDTH//2-300, sd.HEIGHT//2-225 , 600, 450)
+        )
 
 
-# TO DO: 
-    # 'back', 'next', and 'settings' buttons will be added.
+        render_preview_debug_frame(
+            preview_surface,
+            pipeline,
+            car_rotation=self.preview_angle,
+            stack_spread=1
+        )
+
+
+        # draw buttons
+        self.next_btn.draw(surface)
+        self.back_btn.draw(surface)
        
 
 
