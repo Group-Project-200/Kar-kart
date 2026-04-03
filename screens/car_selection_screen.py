fec52b295f800f79595b69277a6f2221cb4dcf75
toni-mass <antoniomassidda6@gmail.com>
2026-04-03 18:56:32 +0900

    fetching from main screens

--- screens/car_selection_screen.py
+++ screens/car_selection_screen.py
@@ -9,7 +9,50 @@ from constants import ScreenPositions as sp
 class CarScreen:
     def __init__(self, manager):
         self.manager = manager     # ALWAYS ADD THE MANAGER
-        self.button1 = Button(sp.CENTER_X, sp.CENTER_Y, 100, 50, "MAP", "map", self.manager)
+
+        # initialize buttons  
+        #* states will be changed to "map" and "start" when they're implemented
+        self.next_btn = CarButton(560, 535, 150, 50, "NEXT", "map", self.manager)
+        self.back_btn = CarButton(240, 535, 150, 50, "BACK", "start", self.manager)
+        
+        
+        # load all images together
+        self.background = pygame.transform.scale( pygame.image.load("cust1.png").convert(), (sp.WIDTH, sp.HEIGHT) )
+
+        def load_car_slices(folder):
+            files = sorted(os.listdir(folder))
+            return [pygame.image.load(os.path.join(folder, f)).convert_alpha() for f in files]
+
+        # load all cars from resources
+        self.car_slices = [
+            load_car_slices("resources/car_01"),
+            load_car_slices("resources/car_02"),
+            load_car_slices("resources/car_03"),
+            load_car_slices("resources/car_04"),
+        ]
+
+        self.pipelines = []
+        for slices in self.car_slices:
+            pipeline = build_render_pipeline(
+                screen_size=(600, 450),   # preview size
+                map_surface=None,
+                image_stack=slices,
+                setup=RenderSetup(
+                    map_zoom=1.0,
+                    # car size
+                    car_zoom=9.0,
+                    pixelation_scale=1.0
+                ),
+                dirs=36
+            )
+            self.pipelines.append(pipeline)
+        
+        self.preview_angle = 0
+
+
+        self.selected = 0  #default car
+
+
 
     def handle_event(self, event):  # use this template for the key detection
 
